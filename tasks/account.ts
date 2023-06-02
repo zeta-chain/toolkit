111e060f955e342eaf68d84d131bee5ffe7f6e9a
Denis Fadeev <denis@fadeev.org>
2023-06-02 11:35:28 +0400

    feat: recover flag

--- tasks/account.ts
+++ tasks/account.ts
@@ -3,14 +3,42 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import * as envfile from "envfile";
 import * as fs from "fs";
 import * as path from "path";
+import { bech32 } from "bech32";
+import { input } from "@inquirer/prompts";
+
+function hexToBech32Address(hexAddress: string, prefix: string): string {
+  const data = Buffer.from(hexAddress.substr(2), "hex");
+  const words = bech32.toWords(data);
+  return bech32.encode(prefix, words);
+}
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   const { ethers } = hre as any;
-  const { privateKey, address, mnemonic } = ethers.Wallet.createRandom();
+  let wallet;
+
+  if (args.recover) {
+    const recovery = await input({
+      message: "Mnemonic or private key:",
+    });
+
+    if (recovery.split(" ").length >= 12) {
+      wallet = ethers.Wallet.fromMnemonic(recovery);
+    } else {
+      wallet = new ethers.Wallet(
+        recovery.startsWith("0x") ? recovery : "0x" + recovery
+      );
+    }
+  } else {
+    wallet = ethers.Wallet.createRandom();
+  }
+
+  const { privateKey, address, mnemonic } = wallet;
+
   console.log(`
-🔑 Private key: ${privateKey}
-🔐 Mnemonic phrase: ${mnemonic.phrase}
-😃 Address: ${address}
+🔑 Private key: ${privateKey}`);
+  mnemonic && console.log(`🔐 Mnemonic phrase: ${mnemonic.phrase}`);
+  console.log(`😃 Address: ${address}
+😃 Bech32 address: ${hexToBech32Address(address, "zeta")}
 `);
 
   if (args.save) {
@@ -24,10 +52,16 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   }
 };
 
-const descTask = `Generates a new account and prints its private key, mnemonic phrase, and address to the console.`;
-const descSaveFlag = `Saves the private key to a '.env' file in the project directory.`;
-
-export const accountTask = task("account", descTask, main).addFlag(
-  "save",
-  descSaveFlag
-);
+export const accountTask = task(
+  "account",
+  `Generates a new account and prints its private key, mnemonic phrase, and address to the console.`,
+  main
+)
+  .addFlag(
+    "save",
+    `Saves the private key to a '.env' file in the project directory.`
+  )
+  .addFlag(
+    "recover",
+    `Recovers a wallet using either a mnemonic or a private key.`
+  );
