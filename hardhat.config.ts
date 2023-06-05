352656f17dd9a576982b5793596df187e3a36266
Denis Fadeev <denis@fadeev.org>
2023-06-05 12:11:41 +0400

    feat: add test contracts and helpers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,14 +2,19 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 import { getHardhatConfigNetworks } from "@zetachain/addresses-tools/dist/networks";
-import "./tasks";
+import "./src/tasks";
 
 dotenv.config();
 const PRIVATE_KEYS =
   process.env.PRIVATE_KEY !== undefined ? [`0x${process.env.PRIVATE_KEY}`] : [];
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      { version: "0.6.6" /** For uniswap v2 */ },
+      { version: "0.8.7" },
+    ],
+  },
   networks: {
     ...getHardhatConfigNetworks(PRIVATE_KEYS),
   },
