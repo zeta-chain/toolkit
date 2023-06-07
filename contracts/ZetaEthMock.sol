6b3386ccb826a311f8f7340bb50930e630ae5e39
Denis Fadeev <denis@fadeev.org>
2023-06-07 20:43:29 +0400

    WIP

--- contracts/ZetaEthMock.sol
+++ contracts/ZetaEthMock.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ZetaEthMock is ERC20("Zeta", "ZETA") {
     constructor(address creator, uint256 initialSupply) {
