c485a19550be865a4bfd9f7fa5caf3c968bc7787
Denis Fadeev <denis@fadeev.org>
2023-06-07 11:52:28 +0400

    prepareData

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -17,14 +17,18 @@ interface EvmSetupResult {
 }
 
 export const prepareData = (contract: string, types: string[], args: any[]) => {
+  const params = prepareParams(types, args);
+  return `${contract}${params.slice(2)}`;
+};
+
+export const prepareParams = (types: string[], args: any[]) => {
   const abiCoder = ethers.utils.defaultAbiCoder;
   for (let i = 0; i < args.length; i++) {
     if (types[i] === "bytes32" && typeof args[i] === "string") {
       args[i] = ethers.utils.hexlify(ethers.utils.zeroPad(args[i], 32));
     }
   }
-  const params = abiCoder.encode(types, args);
-  return `${contract}${params.slice(2)}`;
+  return abiCoder.encode(types, args);
 };
 
 export const addZetaEthLiquidity = async (
