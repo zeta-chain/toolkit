edca0e435d0180cbe02007dc50c7cd5fdaeb8b92
Denis Fadeev <denis@fadeev.org>
2023-06-25 11:32:39 +0400

    refactor: remove condition in prepareParams

--- helpers/evm.ts
+++ helpers/evm.ts
@@ -103,7 +103,7 @@ export const prepareData = (contract: string, types: string[], args: any[]) => {
 export const prepareParams = (types: string[], args: any[]) => {
   const abiCoder = hre.ethers.utils.defaultAbiCoder;
   for (let i = 0; i < args.length; i++) {
-    if (types[i] === "bytes32" && typeof args[i] === "string") {
+    if (types[i] === "bytes32") {
       args[i] = hre.ethers.utils.hexlify(hre.ethers.utils.zeroPad(args[i], 32));
     }
   }
