85d791acace496354119ced058ef9c15277ccf1b
Denis Fadeev <denis@fadeev.org>
2023-07-03 17:16:43 +0400

    feat: cctx tracking

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,2 +1,3 @@
 export * from "./evm";
 export * from "./helpers";
+export * from "./tx";
---
+++ helpers/tx.ts
@@ -0,0 +1,74 @@
+import WebSocket from "ws";
+import axios from "axios";
+import ora from "ora";
+
+const URL = {
+  WSS: "wss://zetachain-athens.blockpi.network/rpc/v1/public/websocket",
+  API: "https://zetachain-athens.blockpi.network/lcd/v1/public/zeta-chain/crosschain",
+};
+
+export const trackCCTX = (inboundTxHash: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    let cctx_index: string;
+    let latest_status: string;
+
+    const spinner = ora(
+      "Looking for the cross-chain transaction (CCTX) on ZetaChain..."
+    ).start();
+
+    const socket = new WebSocket(URL.WSS);
+
+    socket.on("open", () => {
+      const subscribeMessage = {
+        jsonrpc: "2.0",
+        method: "subscribe",
+        params: ["tm.event='NewBlock'"],
+        id: 1,
+      };
+      socket.send(JSON.stringify(subscribeMessage));
+    });
+
+    socket.on("message", async (data: any) => {
+      if (!cctx_index) {
+        try {
+          const url = `${URL.API}/inTxHashToCctx/${inboundTxHash}`;
+          const apiResponse = await axios.get(url);
+          const res = apiResponse?.data?.inTxHashToCctx?.cctx_index;
+          if (res) {
+            cctx_index = res;
+            spinner.succeed(`CCTX hash found: ${cctx_index}\n`);
+            spinner.start("Checking status of the CCTX...");
+          }
+        } catch (error) {}
+      } else {
+        try {
+          const url = `${URL.API}/cctx/${cctx_index}`;
+          const apiResponse = await axios.get(url);
+          const cctx = apiResponse?.data?.CrossChainTx;
+          const { status, status_message } = cctx.cctx_status;
+          if (status != latest_status) {
+            latest_status = status;
+            spinner.info(`Status updated to "${status}": ${status_message}\n`);
+            spinner.start("Checking status of the CCTX...");
+            if (/^(Aborted|Reverted|OutboundMined)$/.test(status)) {
+              socket.close();
+              spinner.succeed("CCTX has been finalized on ZetaChain");
+              resolve();
+              return;
+            }
+          }
+        } catch (error) {}
+      }
+    });
+
+    socket.on("error", (error: any) => {
+      spinner.fail(`WebSocket error: ${error}`);
+      reject(error);
+    });
+
+    socket.on("close", () => {
+      spinner.stop();
+      resolve();
+    });
+  });
+};
