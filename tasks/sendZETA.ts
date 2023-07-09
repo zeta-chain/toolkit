66d4c34782031179a747a04cf9a9cec2d450dd72
Denis Fadeev <denis@fadeev.org>
2023-07-09 13:08:44 +0400

    send zeta between connected chains and zetachain

--- tasks/sendZETA.ts
+++ tasks/sendZETA.ts
@@ -1,7 +1,7 @@
 import { getAddress } from "@zetachain/protocol-contracts";
 import ZetaEthContract from "@zetachain/protocol-contracts/abi/evm/Zeta.eth.sol/ZetaEth.json";
-import ZetaConnector from "@zetachain/protocol-contracts/abi/evm/ZetaConnector.eth.sol/ZetaConnectorEth.json";
-import { ZetaConnectorEth } from "@zetachain/protocol-contracts/typechain-types";
+import ZetaConnectorZEVM from "@zetachain/protocol-contracts/abi/zevm/ConnectorZEVM.sol/ZetaConnectorZEVM.json";
+import ZetaConnectorEth from "@zetachain/protocol-contracts/abi/evm/ZetaConnector.eth.sol/ZetaConnectorEth.json";
 import { parseEther } from "ethers/lib/utils";
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
@@ -9,7 +9,7 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 declare const hre: any;
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
-  let connectorContract: ZetaConnectorEth;
+  let connectorContract: any;
 
   const { ethers } = hre as any;
 
@@ -20,12 +20,14 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     throw new Error("Invalid destination chain");
   }
   const destinationAddress = args.recipient || signer.address;
+
+  const fromZetaChain = hre.network.name === "zeta_testnet";
+
   const connectorAddress = getAddress("connector", hre.network.name as any);
   const zetaTokenAddress = getAddress("zetaToken", hre.network.name as any);
-
   connectorContract = new ethers.Contract(
     connectorAddress,
-    ZetaConnector.abi,
+    fromZetaChain ? ZetaConnectorZEVM.abi : ZetaConnectorEth.abi,
     signer
   );
   const zetaTokenContract = new ethers.Contract(
@@ -34,6 +36,14 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     signer
   );
   const amount = parseEther(args.amount);
+
+  if (fromZetaChain) {
+    await signer.sendTransaction({
+      to: zetaTokenAddress,
+      value: amount,
+    });
+  }
+
   await (
     await zetaTokenContract.connect(signer).approve(connectorAddress, amount)
   ).wait();
@@ -41,12 +51,11 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   const tx = await connectorContract.connect(signer).send({
     destinationAddress,
     destinationChainId,
-    destinationGasLimit: 500000,
+    destinationGasLimit: 5000000,
     message: ethers.utils.arrayify([]),
     zetaParams: ethers.utils.arrayify([]),
     zetaValueAndGas: amount,
   });
-  console.log(tx);
 };
 
 export const sendZETATask = task(
