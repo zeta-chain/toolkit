b7ef4393dd1f732e422608613a5dc7a4db9b770f
Denis Fadeev <denis@fadeev.org>
2023-07-13 21:03:32 +0400

    rename to messaging and omnichain

---
+++ tasks/omnichain.ts
@@ -0,0 +1,99 @@
+import * as fs from "fs";
+import * as handlebars from "handlebars";
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import * as path from "path";
+
+const sanitizeSolidityFunctionName = (str: string): string => {
+  // Remove any character that's not alphanumeric or underscore
+  const cleaned = str.replace(/[^a-zA-Z0-9_]/g, "");
+
+  // If the first character is a digit, prepend with an underscore
+  return cleaned.match(/^\d/) ? `_${cleaned}` : cleaned;
+};
+
+const processTemplates = async (
+  templateDir: string,
+  outputDir: string,
+  data: Record<string, unknown>
+): Promise<void> => {
+  try {
+    templateDir = path.resolve(__dirname, templateDir);
+
+    const files = fs.readdirSync(templateDir);
+
+    for (const file of files) {
+      const templatePath = path.join(templateDir, file);
+
+      // Compiling filename as a template
+      const filenameTemplate = handlebars.compile(file);
+      const filename = filenameTemplate(data);
+
+      // Replacing .hbs extension if the file was a handlebars template
+      const outputPath = path.join(outputDir, filename.replace(".hbs", ""));
+
+      fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+
+      if (fs.lstatSync(templatePath).isDirectory()) {
+        // If file is a directory, recursively process it
+        await processTemplates(templatePath, outputPath, data);
+      } else if (path.extname(file) === ".hbs") {
+        const templateContent = fs.readFileSync(templatePath, "utf-8");
+        const template = handlebars.compile(templateContent);
+        const outputContent = template(data);
+        fs.writeFileSync(outputPath, outputContent);
+      } else {
+        fs.copyFileSync(templatePath, outputPath);
+      }
+    }
+  } catch (error) {
+    console.error(`Error processing templates: ${error}`);
+  }
+};
+
+const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
+  const templateDir = path.resolve(__dirname, "..", "templates", "omnichain");
+  const outputDir = path.resolve(process.cwd());
+  const argsList = args.arguments || [];
+  const names = argsList.map((i: string) => i.split(":")[0]);
+  const types = argsList.map((i: string) => {
+    let parts = i.split(":");
+    // If there's a type and it's not empty, use it; if not, default to "bytes32"
+    let t =
+      parts.length > 1 && parts[1].trim() !== "" ? parts[1].trim() : "bytes32";
+    return t;
+  });
+  const pairs = names.map((v: string, i: string) => [v, types[i]]);
+
+  const data = {
+    args,
+    arguments: { names, pairs, types },
+    contractName: sanitizeSolidityFunctionName(args.name),
+  };
+
+  processTemplates(templateDir, outputDir, data);
+
+  const configPath = path.resolve(process.cwd(), "hardhat.config.ts");
+  let hardhatConfigContents = fs.readFileSync(configPath, "utf8");
+
+  // Add the omnichain tasks to the hardhat.config.ts file
+  ["deploy", "interact"].forEach((task) => {
+    const content = `import "./tasks/${task}";\n`;
+    if (!hardhatConfigContents.includes(content)) {
+      hardhatConfigContents = content + hardhatConfigContents;
+    }
+  });
+
+  fs.writeFileSync(configPath, hardhatConfigContents);
+};
+
+export const omnichainTask = task(
+  "omnichain",
+  "Generate code for an omnichain smart contract",
+  main
+)
+  .addPositionalParam("name", "Name of the contract")
+  .addOptionalVariadicPositionalParam(
+    "arguments",
+    "Arguments for a crosschain call (e.g. dest:address to:bytes32 output:uint256)"
+  );
--- tasks/sendZRC20.ts
+++ tasks/sendZRC20.ts
@@ -7,11 +7,23 @@ declare const hre: any;
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   const { ethers } = hre as any;
-  const [signer] = await ethers.getSigners();
-  const amount = ethers.utils.parseEther(args.amount);
+  // const [signer] = await ethers.getSigners();
+  let amount;
+  try {
+    amount = ethers.utils.parseEther(args.amount);
+  } catch (e) {
+    throw new Error(
+      `${args.amount} is not a number and not a valid value for --amount, ${e}`
+    );
+  }
   let tx;
   if (hre.network.name === "zeta_testnet") {
-    const ZRC20Address = getAddress("zrc20", args.destination);
+    let ZRC20Address;
+    try {
+      ZRC20Address = getAddress("zrc20s", args.destination);
+    } catch (e) {
+      throw new Error(`getAddress: ${e}`);
+    }
     const contract = new ethers.Contract(ZRC20Address, ZRC20.abi, signer);
     await (await contract.connect(signer).approve(ZRC20Address, amount)).wait();
     tx = await contract.connect(signer).withdraw(signer.address, amount);
@@ -21,14 +33,16 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
       to: TSSAddress,
       value: amount,
     });
+  } else {
+    throw new Error("Either --network or --destination should be zeta_testnet");
   }
   console.log(`Transaction hash: ${tx.hash}`);
 };
 
 export const sendZRC20Task = task(
   "send-zrc20",
-  "Send ZRC20 tokens to and from ZetaChain",
+  "Send ZRC-20 tokens to and from ZetaChain",
   main
 )
-  .addParam("amount", "Amount of ZRC20 to send")
+  .addParam("amount", "Amount of ZRC-20 to send")
   .addParam("destination", "Destination chain");
