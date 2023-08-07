2dfae20c105f40b7f675a0a51eb15a265f6800fc
Denis Fadeev <denis@fadeev.org>
2023-08-07 12:17:08 +0400

    feat(cctx): track all outbound txs

--- helpers/tx.ts
+++ helpers/tx.ts
@@ -1,30 +1,23 @@
 import { getEndpoints } from "@zetachain/networks";
 import axios from "axios";
-import moment from "moment";
-import ora from "ora";
 import WebSocket from "ws";
+import Spinnies from "spinnies";
+import clc from "cli-color";
 
-export const trackCCTX = (inboundTxHash: string): Promise<void> => {
+export const trackCCTX = async (inboundTxHash: string): Promise<void> => {
+  const spinnies = new Spinnies();
+
   const API = getEndpoints("cosmos-http", "zeta_testnet")[0]?.url;
   if (API === undefined) {
     throw new Error("getEndpoints: API endpoint not found");
   }
-
   const WSS = getEndpoints("tendermint-ws", "zeta_testnet")[0]?.url;
   if (WSS === undefined) {
     throw new Error("getEndpoints: WSS endpoint not found");
   }
-
   return new Promise((resolve, reject) => {
-    let cctx_index: string;
-    let latest_status: string;
-
-    const spinner = ora(
-      "Looking for the cross-chain transaction (CCTX) on ZetaChain..."
-    ).start();
-
+    let cctxList: any = {};
     const socket = new WebSocket(WSS);
-
     socket.on("open", () => {
       const subscribeMessage = {
         id: 1,
@@ -34,63 +27,104 @@ export const trackCCTX = (inboundTxHash: string): Promise<void> => {
       };
       socket.send(JSON.stringify(subscribeMessage));
     });
-
-    socket.on("message", async (data: any) => {
-      const jsonData = JSON.parse(data);
-      const blockHeight = jsonData?.result?.data?.value?.block?.header?.height;
-      if (!cctx_index) {
+    socket.on("message", async () => {
+      const fetchCCTX = async (hash: string) => {
         try {
-          const url = `${API}/zeta-chain/crosschain/inTxHashToCctx/${inboundTxHash}`;
+          const url = `${API}/zeta-chain/crosschain/inTxHashToCctx/${hash}`;
           const apiResponse = await axios.get(url);
           const res = apiResponse?.data?.inTxHashToCctx?.cctx_index;
           if (res) {
-            cctx_index = res;
-            spinner.succeed(`CCTX hash found: ${cctx_index}\n`);
-            spinner.start(`Checking status of the CCTX...`);
-          }
-        } catch (error) {}
-      } else {
-        try {
-          const url = `${API}/zeta-chain/crosschain/cctx/${cctx_index}`;
-          const apiResponse = await axios.get(url);
-          const cctx = apiResponse?.data?.CrossChainTx;
-          const finalizedBlock =
-            cctx?.inbound_tx_params?.inbound_tx_finalized_zeta_height;
-          const pendingBlocks = blockHeight - finalizedBlock;
-          const { status, status_message } = cctx.cctx_status;
-          if (status != latest_status) {
-            latest_status = status;
-            spinner.info(`Status updated to "${status}": ${status_message}\n`);
-
-            // TODO: disabled until https://github.com/zeta-chain/node/issues/766 is fixed
-            //
-            // if (status === "PendingOutbound" && pendingBlocks > 100) {
-            //   const time = moment
-            //     .duration(pendingBlocks * 5, "seconds")
-            //     .humanize();
-            //   spinner.warn(
-            //     `CCTX is pending for too long (${pendingBlocks} blocks, about ${time})\n`
-            //   );
-            // }
-            spinner.start(`Checking status of the CCTX...`);
-            if (/^(Aborted|Reverted|OutboundMined)$/.test(status)) {
-              socket.close();
-              spinner.succeed("CCTX has been finalized on ZetaChain");
-              resolve();
-              return;
+            if (!cctxList[res]) {
+              cctxList[res] = [];
             }
           }
+          if (!spinnies.spinners[`spinner-${res}`]) {
+            spinnies.add(`spinner-${res}`, {
+              text: `${res}`,
+            });
+          }
         } catch (error) {}
+      };
+      if (Object.keys(cctxList).length === 0) {
+        spinnies.add(`search`, {
+          text: `Looking for cross-chain transactions (CCTXs) on ZetaChain...\n`,
+        });
+        await fetchCCTX(inboundTxHash);
+      }
+      if (Object.keys(cctxList).length > 0) {
+        spinnies.succeed(`search`, {
+          text: `CCTXs on ZetaChain found.\n`,
+        });
+      }
+      for (const txHash in cctxList) {
+        await fetchCCTX(txHash);
+      }
+      if (Object.keys(cctxList).length > 0) {
+        for (const cctxHash in cctxList) {
+          try {
+            const url = `${API}/zeta-chain/crosschain/cctx/${cctxHash}`;
+            const apiResponse = await axios.get(url);
+            const cctx = apiResponse?.data?.CrossChainTx;
+            const { status, status_message } = cctx.cctx_status;
+            const { sender_chain_id } = cctx.inbound_tx_params;
+            const { receiver_chainId } = cctx.outbound_tx_params[0];
+            const tx = {
+              status,
+              status_message,
+              sender_chain_id,
+              receiver_chainId,
+            };
+            const lastCCTX = cctxList[cctxHash][cctxList[cctxHash].length - 1];
+            const isEmpty = cctxList[cctxHash].length === 0;
+            if (isEmpty || lastCCTX?.status !== status) {
+              cctxList[cctxHash].push(tx);
+              const sender = cctxList[cctxHash]?.[0].sender_chain_id;
+              const receiver = cctxList[cctxHash]?.[0].receiver_chainId;
+              const path = cctxList[cctxHash]
+                .map(
+                  (x) =>
+                    `${clc.bold.underline(x.status)} ${
+                      x.status_message && "(" + x.status_message + ")"
+                    }`
+                )
+                .join(" → ");
+              const text = {
+                text: `${cctxHash}: ${sender} → ${receiver}: ${path}`,
+              };
+              const id = `spinner-${cctxHash}`;
+              switch (status) {
+                case "OutboundMined":
+                case "Reverted":
+                  spinnies.succeed(id, text);
+                  break;
+                case "Aborted":
+                  spinnies.fail(id, text);
+                  break;
+                default:
+                  spinnies.update(id, text);
+                  break;
+              }
+            }
+          } catch (error) {}
+        }
+      }
+      if (
+        Object.keys(cctxList).length > 0 &&
+        Object.keys(cctxList)
+          .map((c: any) => {
+            const last = cctxList[c][cctxList[c].length - 1];
+            return last?.status;
+          })
+          .filter((s) => !["OutboundMined", "Aborted", "Reverted"].includes(s))
+          .length === 0
+      ) {
+        socket.close();
       }
     });
-
     socket.on("error", (error: any) => {
-      spinner.fail(`WebSocket error: ${error}`);
       reject(error);
     });
-
     socket.on("close", () => {
-      spinner.stop();
       resolve();
     });
   });
