dec5e2c1bb92bf18eca9a3af987f28329a7137ec
Denis Fadeev <denis@fadeev.org>
2023-08-17 21:11:02 +0400

    refactor: extract fees logic from task into reusable functions (#30)

--- helpers/fees.ts
+++ helpers/fees.ts
@@ -1,73 +1,39 @@
 import { getEndpoints } from "@zetachain/networks";
+import { getHardhatConfigNetworks } from "@zetachain/networks";
 import { getAddress } from "@zetachain/protocol-contracts";
 import ZRC20 from "@zetachain/protocol-contracts/abi/zevm/ZRC20.sol/ZRC20.json";
 import axios from "axios";
-import { formatEther, parseEther } from "ethers/lib/utils";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-
-export type FeeDetails = {
-  gasFee: string;
-  protocolFee: string;
-  totalFee: string;
-};
+import { ethers } from "ethers";
+import { formatEther } from "ethers/lib/utils";
 
 const GAS_LIMIT = 350000;
 
-export const getFee = async (
-  type: "ccm" | "zevm",
-  network: string,
-  hre: HardhatRuntimeEnvironment
-) => {
-  const { ethers } = hre as any;
-  const { url } = hre.config.networks["zeta_testnet"] as any;
-  const provider = new ethers.providers.JsonRpcProvider(url);
-
-  const API = getEndpoints("cosmos-http", "zeta_testnet")[0]?.url;
-  if (!API) {
-    throw new Error("getEndpoints: API endpoint not found");
-  }
-
-  if (type === "zevm") {
-    return fetchZEVMFees(network, provider, hre);
-  } else if (type === "ccm") {
-    return fetchCCMFees(network, hre);
-  }
-};
-
 const formatTo18Decimals = (n: any) => parseFloat(formatEther(n)).toFixed(18);
 
-export const fetchZEVMFees = async (
-  network: string,
-  provider: any,
-  hre: HardhatRuntimeEnvironment
-) => {
+export const fetchZEVMFees = async (network: string) => {
+  const { url } = getHardhatConfigNetworks()["zeta_testnet"] as any;
+
+  const provider = new ethers.providers.JsonRpcProvider(url);
   const btcZRC20 = "0x65a45c57636f9BcCeD4fe193A602008578BcA90b"; // TODO: use getAddress("zrc20", "btc_testnet") when available
   const zrc20Address =
-    network === "btc_testnet" ? btcZRC20 : getAddress("zrc20", network);
+    network === "btc_testnet" ? btcZRC20 : getAddress("zrc20", network as any);
   if (!zrc20Address) return;
 
-  const contract = new hre.ethers.Contract(zrc20Address, ZRC20.abi, provider);
+  const contract = new ethers.Contract(zrc20Address, ZRC20.abi, provider);
+  const [, withdrawGasFee] = await contract.withdrawGasFee();
+  const gasFee = ethers.BigNumber.from(withdrawGasFee);
 
-  const gasFee = hre.ethers.BigNumber.from(
-    (await contract.withdrawGasFee())[1]
-  );
-  const protocolFee = hre.ethers.BigNumber.from(
-    await contract.PROTOCOL_FLAT_FEE()
-  );
-
+  const protocolFee = ethers.BigNumber.from(await contract.PROTOCOL_FLAT_FEE());
   return {
     /* eslint-disable */
-    totalFee: formatTo18Decimals(gasFee.add(protocolFee)),
-    gasFee: formatTo18Decimals(gasFee),
+    totalFee: formatTo18Decimals(gasFee),
+    gasFee: formatTo18Decimals(gasFee.sub(protocolFee)),
     protocolFee: formatTo18Decimals(protocolFee),
     /* eslint-enable */
-  } as FeeDetails;
+  };
 };
 
-export const fetchCCMFees = async (
-  network: string,
-  hre: HardhatRuntimeEnvironment
-) => {
+export const fetchCCMFees = async (network: string) => {
   const API = getEndpoints("cosmos-http", "zeta_testnet")[0]?.url;
   if (!API) {
     throw new Error("getEndpoints: API endpoint not found");
@@ -76,14 +42,34 @@ export const fetchCCMFees = async (
   const url = `${API}/zeta-chain/crosschain/convertGasToZeta?chain=${network}&gasLimit=${GAS_LIMIT}`;
   const { data } = await axios.get(url);
 
-  const gasFee = hre.ethers.BigNumber.from(data.outboundGasInZeta);
-  const protocolFee = hre.ethers.BigNumber.from(data.protocolFeeInZeta);
-
+  const gasFee = ethers.BigNumber.from(data.outboundGasInZeta);
+  const protocolFee = ethers.BigNumber.from(data.protocolFeeInZeta);
   return {
     /* eslint-disable */
     totalFee: formatTo18Decimals(gasFee.add(protocolFee)),
     gasFee: formatTo18Decimals(gasFee),
     protocolFee: formatTo18Decimals(protocolFee),
     /* eslint-enable */
-  } as FeeDetails;
+  };
+};
+
+export const fetchFees = async () => {
+  let fees = {
+    feesCCM: {} as Record<string, any>,
+    feesZEVM: {} as Record<string, any>,
+  };
+
+  const networks = [...Object.keys(getHardhatConfigNetworks()), "btc_testnet"];
+
+  await Promise.all(
+    networks.map(async (n) => {
+      try {
+        const zevmFees = await fetchZEVMFees(n);
+        if (zevmFees) fees.feesZEVM[n] = zevmFees;
+        const ccmFees = await fetchCCMFees(n);
+        if (ccmFees) fees.feesCCM[n] = ccmFees;
+      } catch (err) {}
+    })
+  );
+  return fees;
 };
