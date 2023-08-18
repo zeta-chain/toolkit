3204374d8fc54c8be4150fac21f36fe9cff68615
Denis Fadeev <denis@fadeev.org>
2023-08-18 15:45:50 +0400

    deploy and interact

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -7,3 +7,7 @@ npx hardhat balances
 npx hardhat omnichain Swap targetZRC20:address recipient minAmountOut:uint256
 
 npx hardhat compile --force --no-typechain
+
+npx hardhat deploy --help
+
+npx hardhat interact --help
