46b71ac433ccca399140769725d641c67342fde2
Denis Fadeev <denis@fadeev.org>
2023-08-18 15:53:55 +0400

    hardhat account --save

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -11,3 +11,18 @@ npx hardhat compile --force --no-typechain
 npx hardhat deploy --help
 
 npx hardhat interact --help
+
+npx hardhat account --save
+
+if [[ ! -f .env ]]; then
+    echo ".env file was not created."
+    exit 1
+fi
+
+if grep -E "^PRIVATE_KEY=[a-fA-F0-9]{64}$" .env; then
+    echo "The .env file contains a valid PRIVATE_KEY entry without the 0x prefix."
+else
+    echo "The .env file doesn't contain a valid PRIVATE_KEY entry or it contains the 0x prefix."
+    exit 1
+fi
+
