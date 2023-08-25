806c0cdbe869107ba59fe98b82c774643d07d0b1
Denis Fadeev <denis@fadeev.org>
2023-08-25 19:26:58 +0400

    refactor

--- helpers/tx.ts
+++ helpers/tx.ts
@@ -4,109 +4,126 @@ import clc from "cli-color";
 import Spinnies from "spinnies";
 import WebSocket from "ws";
 
+const getEndpoint = (key: any): string => {
+  const endpoint = getEndpoints(key, "zeta_testnet")[0]?.url;
+  if (!endpoint) {
+    throw new Error(`getEndpoints: ${key} endpoint not found`);
+  }
+  return endpoint;
+};
+
+const fetchCCTX = async (
+  hash: string,
+  spinnies: any,
+  API: string,
+  cctxList: any
+) => {
+  try {
+    const url = `${API}/zeta-chain/crosschain/inTxHashToCctx/${hash}`;
+    const apiResponse = await axios.get(url);
+    const res = apiResponse?.data?.inTxHashToCctx?.cctx_index;
+    res.forEach((cctxHash: any) => {
+      if (cctxHash) {
+        if (!cctxList[cctxHash]) {
+          cctxList[cctxHash] = [];
+        }
+      }
+      if (!spinnies.spinners[`spinner-${cctxHash}`]) {
+        spinnies.add(`spinner-${cctxHash}`, {
+          text: `${cctxHash}`,
+        });
+      }
+    });
+  } catch (error) {}
+};
+
+const SUBSCRIBE_MESSAGE = {
+  id: 1,
+  jsonrpc: "2.0",
+  method: "subscribe",
+  params: ["tm.event='NewBlock'"],
+};
+
+const fetchCCTXData = async (
+  cctxHash: string,
+  spinnies: any,
+  API: string,
+  cctxList: any
+) => {
+  const url = `${API}/zeta-chain/crosschain/cctx/${cctxHash}`;
+  const apiResponse = await axios.get(url);
+  const cctx = apiResponse?.data?.CrossChainTx;
+  const { status, status_message } = cctx.cctx_status;
+  const { sender_chain_id } = cctx.inbound_tx_params;
+  const { receiver_chainId } = cctx.outbound_tx_params[0];
+  const tx = {
+    receiver_chainId,
+    sender_chain_id,
+    status,
+    status_message,
+  };
+  const lastCCTX = cctxList[cctxHash][cctxList[cctxHash].length - 1];
+  const isEmpty = cctxList[cctxHash].length === 0;
+  if (isEmpty || lastCCTX?.status !== status) {
+    cctxList[cctxHash].push(tx);
+    const sender = cctxList[cctxHash]?.[0].sender_chain_id;
+    const receiver = cctxList[cctxHash]?.[0].receiver_chainId;
+    const path = cctxList[cctxHash]
+      .map(
+        (x: any) =>
+          `${clc.bold.underline(x.status)} ${
+            x.status_message && "(" + x.status_message + ")"
+          }`
+      )
+      .join(" → ");
+    const text = {
+      text: `${cctxHash}: ${sender} → ${receiver}: ${path}`,
+    };
+    const id = `spinner-${cctxHash}`;
+    switch (status) {
+      case "OutboundMined":
+      case "Reverted":
+        spinnies.succeed(id, text);
+        break;
+      case "Aborted":
+        spinnies.fail(id, text);
+        break;
+      default:
+        spinnies.update(id, text);
+        break;
+    }
+  }
+};
+
 export const trackCCTX = async (inboundTxHash: string): Promise<void> => {
   const spinnies = new Spinnies();
 
-  const API = getEndpoints("cosmos-http", "zeta_testnet")[0]?.url;
-  if (API === undefined) {
-    throw new Error("getEndpoints: API endpoint not found");
-  }
-  const WSS = getEndpoints("tendermint-ws", "zeta_testnet")[0]?.url;
-  if (WSS === undefined) {
-    throw new Error("getEndpoints: WSS endpoint not found");
-  }
+  const API = getEndpoint("cosmos-http");
+  const WSS = getEndpoint("tendermint-ws");
+
   return new Promise((resolve, reject) => {
     let cctxList: any = {};
     const socket = new WebSocket(WSS);
-    socket.on("open", () => {
-      const subscribeMessage = {
-        id: 1,
-        jsonrpc: "2.0",
-        method: "subscribe",
-        params: ["tm.event='NewBlock'"],
-      };
-      socket.send(JSON.stringify(subscribeMessage));
-    });
+    socket.on("open", () => socket.send(JSON.stringify(SUBSCRIBE_MESSAGE)));
     socket.on("message", async () => {
-      const fetchCCTX = async (hash: string) => {
-        try {
-          const url = `${API}/zeta-chain/crosschain/inTxHashToCctx/${hash}`;
-          const apiResponse = await axios.get(url);
-          const res = apiResponse?.data?.inTxHashToCctx?.cctx_index;
-          res.forEach((cctxHash: any) => {
-            if (cctxHash) {
-              if (!cctxList[cctxHash]) {
-                cctxList[cctxHash] = [];
-              }
-            }
-            if (!spinnies.spinners[`spinner-${cctxHash}`]) {
-              spinnies.add(`spinner-${cctxHash}`, {
-                text: `${cctxHash}`,
-              });
-            }
-          });
-        } catch (error) {}
-      };
       if (Object.keys(cctxList).length === 0) {
         spinnies.add(`search`, {
           text: `Looking for cross-chain transactions (CCTXs) on ZetaChain...\n`,
         });
-        await fetchCCTX(inboundTxHash);
-      }
-      if (Object.keys(cctxList).length > 0) {
-        spinnies.succeed(`search`, {
-          text: `CCTXs on ZetaChain found.\n`,
-        });
+        await fetchCCTX(inboundTxHash, spinnies, API, cctxList);
       }
       for (const txHash in cctxList) {
-        await fetchCCTX(txHash);
+        await fetchCCTX(txHash, spinnies, API, cctxList);
       }
       if (Object.keys(cctxList).length > 0) {
+        if (spinnies.spinners["search"]) {
+          spinnies.succeed(`search`, {
+            text: `CCTXs on ZetaChain found.\n`,
+          });
+        }
         for (const cctxHash in cctxList) {
           try {
-            const url = `${API}/zeta-chain/crosschain/cctx/${cctxHash}`;
-            const apiResponse = await axios.get(url);
-            const cctx = apiResponse?.data?.CrossChainTx;
-            const { status, status_message } = cctx.cctx_status;
-            const { sender_chain_id } = cctx.inbound_tx_params;
-            const { receiver_chainId } = cctx.outbound_tx_params[0];
-            const tx = {
-              receiver_chainId,
-              sender_chain_id,
-              status,
-              status_message,
-            };
-            const lastCCTX = cctxList[cctxHash][cctxList[cctxHash].length - 1];
-            const isEmpty = cctxList[cctxHash].length === 0;
-            if (isEmpty || lastCCTX?.status !== status) {
-              cctxList[cctxHash].push(tx);
-              const sender = cctxList[cctxHash]?.[0].sender_chain_id;
-              const receiver = cctxList[cctxHash]?.[0].receiver_chainId;
-              const path = cctxList[cctxHash]
-                .map(
-                  (x) =>
-                    `${clc.bold.underline(x.status)} ${
-                      x.status_message && "(" + x.status_message + ")"
-                    }`
-                )
-                .join(" → ");
-              const text = {
-                text: `${cctxHash}: ${sender} → ${receiver}: ${path}`,
-              };
-              const id = `spinner-${cctxHash}`;
-              switch (status) {
-                case "OutboundMined":
-                case "Reverted":
-                  spinnies.succeed(id, text);
-                  break;
-                case "Aborted":
-                  spinnies.fail(id, text);
-                  break;
-                default:
-                  spinnies.update(id, text);
-                  break;
-              }
-            }
+            fetchCCTXData(cctxHash, spinnies, API, cctxList);
           } catch (error) {}
         }
       }
