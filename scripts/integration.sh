d85be4c1b4a839d88c8240a3503bfa051653a32d
Denis Fadeev <denis@fadeev.org>
2023-08-26 16:39:01 +0400

    deploy

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -23,6 +23,7 @@ fi
 
 if grep -E "^PRIVATE_KEY=[a-fA-F0-9]{64}$" .env; then
     echo "The .env file contains a valid PRIVATE_KEY entry without the 0x prefix."
+    rm .env
 else
     echo "The .env file doesn't contain a valid PRIVATE_KEY entry or it contains the 0x prefix."
     exit 1
