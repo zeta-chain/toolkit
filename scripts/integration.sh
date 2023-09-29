3b750413c1217ccf2f69a47ab49f9f71709437e6
Denis Fadeev <denis@fadeev.org>
2023-09-29 11:30:22 +0400

    set fee to 3 ZETA

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -77,7 +77,7 @@ CCM_FEE=$(npx hardhat fees --json | jq -r ".feesCCM.mumbai_testnet.totalFee")
 
 echo "CCM fee: $CCM_FEE"
 
-CCM_TX=$(npx hardhat interact --network goerli_testnet --contract $CCM_CONTRACT --message "Hello World" --destination mumbai_testnet --amount $CCM_FEE --json | jq -r '.hash')
+CCM_TX=$(npx hardhat interact --network goerli_testnet --contract $CCM_CONTRACT --message "Hello World" --destination mumbai_testnet --amount 3 --json | jq -r '.hash')
 
 echo "CCM TX hash: $CCM_TX"
 
