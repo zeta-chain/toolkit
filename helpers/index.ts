772570e382f63f5d213e3184a1a1db3e5e2c66df
Denis Fadeev <denis@fadeev.org>
2023-10-24 00:58:47 +0800

    lint

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,6 +1,6 @@
 export * from "./balances";
 export * from "./fees";
 export * from "./pools";
+export * from "./prepare";
 export * from "./sendZETA";
 export * from "./tx";
-export * from "./prepare";
