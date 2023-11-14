c0a6e5ffd0a25893b3841ac84d3db0990f58cad7
Denis Fadeev <denis@fadeev.org>
2023-11-14 21:22:07 +0300

    move bytesMemoryToAddress next to bytesToAddress

--- contracts/BytesHelperLib.sol
+++ contracts/BytesHelperLib.sol
@@ -12,6 +12,19 @@ library BytesHelperLib {
         }
     }
 
+    function bytesMemoryToAddress(
+        bytes memory data,
+        uint256 offset
+    ) internal pure returns (address output) {
+        bytes memory b = new bytes(20);
+        for (uint256 i = 0; i < 20; i++) {
+            b[i] = data[i + offset];
+        }
+        assembly {
+            output := mload(add(b, 20))
+        }
+    }
+
     function bytesToUint32(
         bytes calldata data,
         uint256 offset
@@ -38,18 +51,5 @@ library BytesHelperLib {
         }
 
         return bech32Bytes;
-    }
-
-    function bytesToAddress(
-        bytes memory data,
-        uint256 offset
-    ) internal pure returns (address output) {
-        bytes memory b = new bytes(20);
-        for (uint256 i = 0; i < 20; i++) {
-            b[i] = data[i + offset];
-        }
-        assembly {
-            output := mload(add(b, 20))
-        }
     }
 }
