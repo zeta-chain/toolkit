4cfbbfe3cf6709f1841288f497cff1c1c4b8eee6
Denis Fadeev <denis@fadeev.org>
2024-02-20 09:20:26 +0300

    Fix bytesMemoryToAddress (#90)

--- contracts/BytesHelperLib.sol
+++ contracts/BytesHelperLib.sol
@@ -16,12 +16,8 @@ library BytesHelperLib {
         bytes memory data,
         uint256 offset
     ) internal pure returns (address output) {
-        bytes memory b = new bytes(20);
-        for (uint256 i = 0; i < 20; i++) {
-            b[i] = data[i + offset];
-        }
         assembly {
-            output := mload(add(b, 20))
+            output := mload(add(add(data, offset), 32))
         }
     }
 
