39a30ed3db08c6110f6cc5c9a5703e6b50cd43d4
Denis Fadeev <denis@fadeev.org>
2024-02-20 17:52:22 +0300

    client workspaces

--- helpers/getSupportedChains.ts
+++
@@ -1,9 +0,0 @@
-import { ZetaChainClient } from "./client";
-
-export const getSupportedChains = async function (this: ZetaChainClient) {
-  const api = await this.getEndpoints("cosmos-http", `zeta_${this.network}`);
-  const endpoint = `${api}/zeta-chain/observer/supportedChains`;
-  const response = await fetch(endpoint);
-  const data = await response.json();
-  return data.chains;
-};
--- helpers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./balances";
-export * from "./client";
-export * from "./fees";
-export * from "./pools";
-export * from "./prepare";
-export * from "./sendZETA";
-export * from "./sendZRC20";
