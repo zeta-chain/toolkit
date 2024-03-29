865c1e991220cb5b12bf968a2c67015ef7038953
lukema95 <867273263@qq.com>
2024-03-29 16:06:20 +0800

    optimize code style

--- contracts/shared/TestUniswapRouter.sol
+++ contracts/shared/TestUniswapRouter.sol
@@ -150,15 +150,15 @@ contract TestUniswapRouter {
 
     // **** SWAP ****
     // requires the initial amount to have already been sent to the first pair
-    function _swap(uint[] memory amounts, address[] memory path, address _to) internal virtual {
+    function _swap(uint[] memory amounts, address[] memory path, address to) internal virtual {
         for (uint i; i < path.length - 1; i++) {
             (address input, address output) = (path[i], path[i + 1]);
             (address token0,) = UniswapV2Library.sortTokens(input, output);
             uint amountOut = amounts[i + 1];
             (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
-            address to = i < path.length - 2 ? UniswapV2Library.pairFor(factory, output, path[i + 2]) : _to;
+            address toAddr = i < path.length - 2 ? UniswapV2Library.pairFor(factory, output, path[i + 2]) : to;
             IUniswapV2Pair(UniswapV2Library.pairFor(factory, input, output)).swap(
-                amount0Out, amount1Out, to, new bytes(0)
+                amount0Out, amount1Out, toAddr, new bytes(0)
             );
         }
     }
