59450c4da2906ebfd2d1295d72f25c6670a2247d
Denis Fadeev <denis@fadeev.org>
2024-04-10 12:54:54 +0800

    feat: added config to verify contracts on Blockscout

--- packages/client/src/getHardhatConfig.ts
+++ packages/client/src/getHardhatConfig.ts
@@ -39,8 +39,34 @@ export const getHardhatConfig = function ({ accounts }: any) {
     if (accounts.every((e: string) => e)) config[network].accounts = accounts;
   }
 
+  const etherscan = {
+    apiKey: {
+      zeta_testnet: "YOUR_KEY_HERE",
+      zeta_mainnet: "YOUR_KEY_HERE",
+    },
+    customChains: [
+      {
+        chainId: 7001,
+        network: "zeta_testnet",
+        urls: {
+          apiURL: "https://zetachain-athens-3.blockscout.com/api",
+          browserURL: "https://zetachain-athens-3.blockscout.com",
+        },
+      },
+      {
+        chainId: 7000,
+        network: "zeta_mainnet",
+        urls: {
+          apiURL: "https://zetachain.blockscout.com/api",
+          browserURL: "https://zetachain.blockscout.com",
+        },
+      },
+    ],
+  };
+
   return {
     networks: { ...config, hardhat },
     solidity: "0.8.7",
+    etherscan,
   };
 };
