59aa44c42e9a7e7182c83f3d342da24b898c91c0
Denis Fadeev <denis@fadeev.org>
2024-04-10 11:52:05 +0800

    remove duplicate export

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -6,7 +6,6 @@ export * from "./getEndpoint";
 export * from "./getFees";
 export * from "./getForeignCoins";
 export * from "./getHardhatConfig";
-export * from "./getHardhatConfig";
 export * from "./getPools";
 export * from "./getSupportedChains";
 export * from "./prepareData";
