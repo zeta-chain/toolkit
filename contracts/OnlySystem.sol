424f64ba663f87ca0390546f7b3e3c341b5ddbb2
Denis Fadeev <denis@fadeev.org>
2024-05-13 20:52:35 +0400

    feat: generate omnichain contracts with new OnlySystem by default (#133)

--- contracts/OnlySystem.sol
+++ contracts/OnlySystem.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
+
 contract OnlySystem {
     error OnlySystemContract(string);
 
-    modifier onlySystem(address systemContract) {
+    modifier onlySystem(SystemContract systemContract) {
         if (msg.sender != address(systemContract)) {
             revert OnlySystemContract(
                 "Only system contract can call this function"
