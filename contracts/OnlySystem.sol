d4ecf31247de50062826a9c9a977fd6c5eff9f1c
Denis Fadeev <denis@fadeev.org>
2024-05-21 12:59:36 +0300

    moved system contract logic into onlysystem

--- contracts/OnlySystem.sol
+++ contracts/OnlySystem.sol
@@ -4,16 +4,24 @@ pragma solidity ^0.8.7;
 import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
 
 contract OnlySystem {
-    address internal systemContractAddress;
+    SystemContract internal systemContract;
+    mapping(uint256 => address) private systemContracts;
 
     error OnlySystemContract(string);
 
-    constructor(address _systemContractAddress) {
-        systemContractAddress = _systemContractAddress;
+    constructor() {
+        address _systemContractAddress = _initializeSystemContracts();
+        systemContract = SystemContract(_systemContractAddress);
+    }
+
+    function _initializeSystemContracts() private returns (address) {
+        systemContracts[7000] = 0x91d18e54DAf4F677cB28167158d6dd21F6aB3921;
+        systemContracts[7001] = 0xEdf1c3275d13489aCdC6cD6eD246E72458B8795B;
+        return systemContracts[block.chainid];
     }
 
     modifier onlySystem() {
-        if (msg.sender != systemContractAddress) {
+        if (msg.sender != address(systemContract)) {
             revert OnlySystemContract(
                 "Only system contract can call this function"
             );
