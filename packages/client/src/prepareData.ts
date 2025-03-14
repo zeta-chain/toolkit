2a5826bb1b079888433de5205ab650a119a80805
Hernan Clich <hernan.g.clich@gmail.com>
2025-03-14 16:00:13 -0300

    Add types to prepare data fn

--- packages/client/src/prepareData.ts
+++ packages/client/src/prepareData.ts
@@ -1,15 +1,29 @@
-import { ethers } from "ethers";
+import { BytesLike, ethers } from "ethers";
+import { isBytesLike } from "ethers/lib/utils";
 
-export const prepareData = (contract: string, types: string[], args: any[]) => {
+export type SupportedArgType =
+  | string
+  | bigint
+  | boolean
+  | Uint8Array
+  | BytesLike;
+
+export const prepareData = (
+  contract: string,
+  types: string[],
+  args: SupportedArgType[]
+) => {
   const params = prepareParams(types, args);
   return `${contract}${params.slice(2)}`;
 };
 
-export const prepareParams = (types: string[], args: any[]) => {
+export const prepareParams = (types: string[], args: SupportedArgType[]) => {
   const abiCoder = ethers.utils.defaultAbiCoder;
   for (let i = 0; i < args.length; i++) {
-    if (types[i] === "bytes32") {
-      args[i] = ethers.utils.hexlify(ethers.utils.zeroPad(args[i], 32));
+    if (types[i] === "bytes32" && isBytesLike(args[i])) {
+      args[i] = ethers.utils.hexlify(
+        ethers.utils.zeroPad(args[i] as BytesLike, 32)
+      );
     }
   }
   return abiCoder.encode(types, args);
