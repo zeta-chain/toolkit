65845a889707083910ce168ca523ced01a01ba8b
Hernan Clich <hernan.g.clich@gmail.com>
2025-03-14 19:09:11 -0300

    Added types to faucet

--- packages/tasks/src/faucet.ts
+++ packages/tasks/src/faucet.ts
@@ -1,12 +1,15 @@
 // @ts-expect-error @description Could not find a declaration file for drip module
 import { drip } from "@zetachain/faucet-cli/dist/commands/drip";
 import * as dotenv from "dotenv";
-import { ethers } from "ethers";
+import { ethers, utils } from "ethers";
 import { task } from "hardhat/config";
+import { z } from "zod";
 
 import { walletError } from "./balances";
 
 dotenv.config();
+
+const typedDripFunction = drip as (args: { address: string }) => Promise<void>;
 
 const faucetError = `
 * Alternatively, you can request tokens on any address
@@ -15,21 +18,37 @@ const faucetError = `
   npx hardhat faucet --address <wallet_address>
 `;
 
-const getRecipientAddress = (args: any) => {
-  if (args.address) {
-    return args.address;
+const getRecipientAddress = (address: string) => {
+  if (address) {
+    return address;
   } else if (process.env.PRIVATE_KEY) {
     return new ethers.Wallet(process.env.PRIVATE_KEY).address;
   } else {
-    console.error(walletError + faucetError);
-    throw new Error();
+    throw new Error(walletError + faucetError);
   }
 };
 
-const main = async (args: any) => {
+const evmAddressSchema = z
+  .string()
+  .refine((val) => utils.isAddress(val), "Must be a valid EVM address");
+
+const faucetArgsSchema = z.object({
+  address: evmAddressSchema,
+});
+
+type FaucetArgs = z.infer<typeof faucetArgsSchema>;
+
+const main = async (args: FaucetArgs) => {
+  const { data: parsedArgs, success, error } = faucetArgsSchema.safeParse(args);
+
+  if (!success) {
+    console.error("Invalid arguments:", error?.message);
+    return;
+  }
+
   try {
-    const address = getRecipientAddress(args);
-    await drip({ address });
+    const address = getRecipientAddress(parsedArgs.address);
+    await typedDripFunction({ address });
   } catch (error) {
     console.error(error);
   }
