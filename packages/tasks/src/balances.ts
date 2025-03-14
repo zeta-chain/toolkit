477748bfa89a2ed72255ca218d9a09af62e87bfc
Hernan Clich <hernan.g.clich@gmail.com>
2025-03-14 16:13:44 -0300

    Fix balances types

--- packages/tasks/src/balances.ts
+++ packages/tasks/src/balances.ts
@@ -1,10 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { Keypair } from "@solana/web3.js";
 import bs58 from "bs58";
 import * as dotenv from "dotenv";
 import { ethers } from "ethers";
 import { task } from "hardhat/config";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
 import ora from "ora";
+import { z } from "zod";
 
 import { ZetaChainClient } from "../../client/src/";
 import { bitcoinAddress } from "./bitcoinAddress";
@@ -35,12 +36,35 @@ const balancesError = `
   npx hardhat balances --evm <evm_address> --solana <solana_address> --bitcoin <bitcoin_address>
 `;
 
-const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
+const solanaKeySchema = z.array(z.number());
+
+const balancesArgsSchema = z.object({
+  bitcoin: z.string().optional(),
+  evm: z.string().optional(),
+  json: z.boolean().optional(),
+  mainnet: z.boolean().optional(),
+  solana: z.string().optional(),
+});
+
+type BalancesArgs = z.infer<typeof balancesArgsSchema>;
+
+const main = async (args: BalancesArgs) => {
+  const {
+    data: parsedArgs,
+    success,
+    error,
+  } = balancesArgsSchema.safeParse(args);
+
+  if (!success) {
+    console.error("Invalid arguments:", error?.message);
+    return;
+  }
+
   const client = new ZetaChainClient({
-    network: args.mainnet ? "mainnet" : "testnet",
+    network: parsedArgs.mainnet ? "mainnet" : "testnet",
   });
   const spinner = ora("Fetching balances...");
-  if (!args.json) {
+  if (!parsedArgs.json) {
     spinner.start();
   }
 
@@ -52,14 +76,14 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   let btcAddress: string | undefined;
   let solanaAddress: string | undefined;
 
-  if (args.evm) {
-    evmAddress = args.evm;
+  if (parsedArgs.evm) {
+    evmAddress = parsedArgs.evm;
   }
-  if (args.solana) {
-    solanaAddress = args.solana;
+  if (parsedArgs.solana) {
+    solanaAddress = parsedArgs.solana;
   }
-  if (args.bitcoin) {
-    btcAddress = args.bitcoin;
+  if (parsedArgs.bitcoin) {
+    btcAddress = parsedArgs.bitcoin;
   }
 
   if (!evmAddress && !solanaAddress && !btcAddress) {
@@ -74,9 +98,11 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     }
     if (solanaKey) {
       try {
+        const parsedKey = solanaKeySchema.parse(JSON.parse(solanaKey));
+
         if (solanaKey.startsWith("[") && solanaKey.endsWith("]")) {
           solanaAddress = Keypair.fromSecretKey(
-            Uint8Array.from(JSON.parse(solanaKey))
+            Uint8Array.from(parsedKey)
           ).publicKey.toString();
         } else {
           solanaAddress = Keypair.fromSecretKey(
@@ -90,7 +116,10 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
       }
     }
     if (btcKey) {
-      btcAddress = bitcoinAddress(btcKey, args.mainnet ? "mainnet" : "testnet");
+      btcAddress = bitcoinAddress(
+        btcKey,
+        parsedArgs.mainnet ? "mainnet" : "testnet"
+      );
     }
     if (!solanaKey && !btcKey && !evmKey) {
       {
@@ -101,22 +130,21 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     }
   }
 
-  let balances = (await client.getBalances({
+  let balances = await client.getBalances({
     btcAddress,
     evmAddress,
     solanaAddress,
-  })) as any;
+  });
 
-  if (args.json) {
+  if (parsedArgs.json) {
     console.log(JSON.stringify(balances, null, 2));
   } else {
     spinner.stop();
+
     console.log(`
-EVM: ${evmAddress} ${btcAddress ? `\nBitcoin: ${btcAddress}` : ""} ${
-      solanaAddress ? `\nSolana: ${solanaAddress}` : ""
-    }
+EVM: ${evmAddress} ${btcAddress ? `\nBitcoin: ${btcAddress}` : ""} ${solanaAddress ? `\nSolana: ${solanaAddress}` : ""}
     `);
-    balances = balances.sort((a: any, b: any) => {
+    balances = balances.sort((a, b) => {
       if (a?.chain_name === undefined && b?.chain_name === undefined) return 0;
       if (a?.chain_name === undefined) return 1;
       if (b?.chain_name === undefined) return -1;
@@ -124,15 +152,13 @@ EVM: ${evmAddress} ${btcAddress ? `\nBitcoin: ${btcAddress}` : ""} ${
       return a.chain_name.localeCompare(b.chain_name);
     });
 
-    balances = balances.map((balance: any) => ({
-      /* eslint-disable */
+    const parsedBalances = balances.map((balance) => ({
+      Amount: `${parseFloat(balance.balance).toFixed(6)}`,
       Chain: balance.chain_name,
       Token: balance.symbol,
       Type: balance.coin_type,
-      Amount: `${parseFloat(balance.balance).toFixed(6)}`,
-      /* eslint-enable */
     }));
-    console.table(balances);
+    console.table(parsedBalances);
   }
 };
 
