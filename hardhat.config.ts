e8f745fb37bec89c737e6edd8c092acc74219a8f
Hernan Clich <hernan.g.clich@gmail.com>
2025-03-19 12:40:58 -0300

    Initial setup ok

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import "./packages/tasks/src";
 import * as dotenv from "dotenv";
 import type { HardhatUserConfig } from "hardhat/config";
 
-import { getHardhatConfig } from "./packages/client/src";
+import { getHardhatConfig } from "./utils";
 
 dotenv.config();
 
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
 export * from "./bitsize";
+export * from "./getHardhatConfig";
 export * from "./parseAbiValues";
 export * from "./toHexString";
