bad4b1eb4c15e5d2e0e817f0303909e678425553
Denis Fadeev <denis@fadeev.org>
2025-04-09 17:33:40 +0300

    command

---
+++ packages/commands/src/suiEncode.ts
@@ -0,0 +1,29 @@
+import { Command } from "commander";
+
+import { EncodeOptions, suiEncode } from "../../client/src/suiEncode";
+
+const main = async (options: EncodeOptions) => {
+  try {
+    if (options.objects) {
+      options.objects = options.objects.flatMap((obj) => obj.split(","));
+    }
+    const encoded = suiEncode(options);
+    console.log(encoded);
+  } catch (error) {
+    console.error("Error encoding SUI payload:", error);
+    process.exit(1);
+  }
+};
+
+export const suiEncodeCommand = new Command("encode")
+  .description("Encode payload data for SUI")
+  .requiredOption("--data <data>", "Data to encode")
+  .option(
+    "--type-arguments <typeArguments...>",
+    "Type arguments for the encoding"
+  )
+  .option(
+    "--objects <objects...>",
+    "Objects to include in the encoding (comma-separated)"
+  )
+  .action(main);
