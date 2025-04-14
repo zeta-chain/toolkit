2efc9c02914e551ab2a5dc79003a266fd7495d85
Denis Fadeev <denis@fadeev.org>
2025-04-14 18:29:05 +0300

    command interface

--- packages/commands/src/bitcoin/encode.ts
+++ packages/commands/src/bitcoin/encode.ts
@@ -9,33 +9,19 @@ interface EncodeOptions {
   opCode?: string;
   receiver: string;
   revertAddress: string;
-  types: string;
+  types: string[];
   values: string[];
 }
 
 const main = (options: EncodeOptions) => {
-  let types: string[];
-  let values: unknown[];
-  try {
-    types = JSON.parse(options.types) as string[];
-    values = JSON.parse(options.values[0]) as unknown[];
-    if (!Array.isArray(types)) {
-      throw new Error("Types must be an array");
-    }
-    if (!Array.isArray(values)) {
-      throw new Error("Values must be an array");
-    }
-    if (types.length !== values.length) {
-      throw new Error("Number of types must match number of values");
-    }
-  } catch (e) {
-    throw new Error(
-      "Invalid format. Expected JSON arrays for both types and values"
-    );
+  if (options.types.length !== options.values.length) {
+    throw new Error("Number of types must match number of values");
   }
 
-  // Encode using ABI
-  const encodedPayload = new ethers.AbiCoder().encode(types, values);
+  const encodedPayload = new ethers.AbiCoder().encode(
+    options.types,
+    options.values
+  );
   const payloadBuffer = Buffer.from(trimOx(encodedPayload), "hex");
 
   // Parse opCode and format as numbers
@@ -66,10 +52,7 @@ export const encodeCommand = new Command()
   .name("encode")
   .description("Encode data for Bitcoin transactions using ABI encoding")
   .requiredOption("-r, --receiver <address>", "Receiver address")
-  .requiredOption(
-    "-t, --types <types>",
-    'ABI types as JSON array (e.g. \'["string","uint256"]\')'
-  )
+  .requiredOption("-t, --types <types...>", "ABI types (e.g. string uint256)")
   .requiredOption("-v, --values <values...>", "Values corresponding to types")
   .requiredOption("-a, --revert-address <address>", "Bitcoin revert address")
   .option("-o, --op-code <code>", "Operation code (0-3)", "1")
