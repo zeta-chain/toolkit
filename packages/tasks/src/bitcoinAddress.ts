898e7060773518f43a4dc4b689909dc8d4b47a45
Hernan Clich <hernan.g.clich@gmail.com>
2025-04-15 12:29:41 -0300

    Restore deprecated fn

---
+++ packages/tasks/src/bitcoinAddress.ts
@@ -0,0 +1,22 @@
+import * as bitcoin from "bitcoinjs-lib";
+import ECPairFactory from "ecpair";
+import * as ecc from "tiny-secp256k1";
+
+/**
+ * @deprecated This function is deprecated. Please use generateBitcoinAddress instead.
+ */
+export const bitcoinAddress = (pk: string, network: "mainnet" | "testnet") => {
+  const bitcoinNetwork =
+    network === "testnet" ? bitcoin.networks.testnet : bitcoin.networks.bitcoin;
+
+  const ECPair = ECPairFactory(ecc);
+  const key = ECPair.fromPrivateKey(Buffer.from(pk, "hex"), {
+    network: bitcoinNetwork,
+  });
+  const { address } = bitcoin.payments.p2wpkh({
+    network: bitcoinNetwork,
+    pubkey: key.publicKey,
+  });
+  if (!address) throw new Error("Unable to generate bitcoin address");
+  return address;
+};
