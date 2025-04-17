fd67f5dc70b5f4faed27b60971a402a2c8e81191
Hernan Clich <hernan.g.clich@gmail.com>
2025-04-17 12:04:34 -0300

    Derived types from schemas

--- types/accounts.types.ts
+++ types/accounts.types.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+import { evmAddressSchema } from "../types/shared.schema";
+
 export interface AccountData {
   [key: string]: string | undefined;
 }
@@ -10,18 +12,6 @@ export interface AccountInfo {
   type: string;
 }
 
-export interface EVMAccountData {
-  address: string;
-  mnemonic?: string;
-  name?: string;
-  privateKey: string;
-}
-
-export interface SolanaAccountData {
-  name?: string;
-  publicKey: string;
-  secretKey: string;
-}
 export interface AccountDetails {
   [key: string]: string;
 }
@@ -30,7 +20,7 @@ export const AvailableAccountTypes = ["evm", "solana"] as const;
 
 // Define schemas for account data types
 const evmAccountDataSchema = z.object({
-  address: z.string(),
+  address: evmAddressSchema,
   mnemonic: z.string().optional(),
   name: z.string().optional(),
   privateKey: z.string(),
@@ -41,6 +31,9 @@ const solanaAccountDataSchema = z.object({
   publicKey: z.string(),
   secretKey: z.string(),
 });
+
+export type EVMAccountData = z.infer<typeof evmAccountDataSchema>;
+export type SolanaAccountData = z.infer<typeof solanaAccountDataSchema>;
 
 // Union schema for both account types
 export const accountDataSchema = z.union([
