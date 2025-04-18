38633c44220c0074128a20bdb94e6188a0f19d8d
Denis Fadeev <denis@fadeev.org>
2025-04-18 18:21:36 +0300

    feat: create account named default by default

--- packages/commands/src/accounts/create.ts
+++ packages/commands/src/accounts/create.ts
@@ -108,7 +108,7 @@ export const createAccountsCommand = new Command("create")
       AvailableAccountTypes
     )
   )
-  .requiredOption("--name <name>", "Account name")
+  .requiredOption("--name <name>", "Account name", "default")
   .action(async (opts) => {
     const validated = validateAndParseSchema(opts, createAccountOptionsSchema, {
       exitOnError: true,
