25c16e9ccf9f83efb27d889a32305f8a7d7a7309
Hernan Clich <hernan.g.clich@gmail.com>
2025-04-28 13:46:03 -0300

    Exposing balances command

--- packages/commands/src/balances/balances.ts
+++ packages/commands/src/balances/balances.ts
@@ -3,14 +3,14 @@ import { Command, Option } from "commander";
 import ora from "ora";
 import { z } from "zod";
 
-import { accountNameSchema } from "../../../types/accounts.types";
+import { accountNameSchema } from "../../../../types/accounts.types";
 import {
   resolveBitcoinAddress,
   resolveEvmAddress,
   resolveSolanaAddress,
-} from "../../../utils/addressResolver";
-import { formatAddresses, formatBalances } from "../../../utils/formatting";
-import { ZetaChainClient } from "../../client/src/client";
+} from "../../../../utils/addressResolver";
+import { formatAddresses, formatBalances } from "../../../../utils/formatting";
+import { ZetaChainClient } from "../../../client/src/client";
 
 /**
  * @todo (Hernan): We need to change this to the account command once we have it.
