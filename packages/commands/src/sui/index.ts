a17f4fc9daf8c15f4b4f5ed452af9e837f17c76a
Denis Fadeev <denis@fadeev.org>
2025-04-30 15:07:47 +0300

    index

--- packages/commands/src/sui/index.ts
+++ packages/commands/src/sui/index.ts
@@ -1,7 +1,9 @@
 import { Command } from "commander";
 
 import { encodeCommand } from "./encode";
+import { depositCommand } from "./deposit";
 
 export const suiCommand = new Command("sui").description("Sui commands");
 
 suiCommand.addCommand(encodeCommand);
+suiCommand.addCommand(depositCommand);
