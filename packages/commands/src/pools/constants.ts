2985c00318f98e50065a098b0a3211a63bd24cad
Denis Fadeev <denis@fadeev.org>
2025-05-05 15:56:37 +0300

    updated factory

--- packages/commands/src/pools/constants.ts
+++ packages/commands/src/pools/constants.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_RPC =
   "https://zetachain-athens.g.allthatnode.com/archive/evm";
-export const DEFAULT_FACTORY = "0x7E032E349853178C233a2560d9Ea434ac82228e0";
+export const DEFAULT_FACTORY = "0xdE7BecC9e313Ef0fD684A2826d5C506Ae75644Df";
 export const DEFAULT_WZETA = "0x5F0b1a82749cb4E2278EC87F8BF6B618dC71a8bf";
 export const DEFAULT_FEE = 3000; // 0.3%
 export const DEFAULT_POSITION_MANAGER =
-  "0xFc5D90f650cf46Cecf96C66a4993f97D2a49f93B";
+  "0xB497bdD833f1395AB8cF50591Bd313d607465d70";
