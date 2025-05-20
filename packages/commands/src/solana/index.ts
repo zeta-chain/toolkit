56536e995394332176d0f57a5610fc153a6c8d7b
Hernan Clich <hernan.g.clich@gmail.com>
2025-05-20 11:36:33 -0300

    Chaining commands

--- packages/commands/src/solana/index.ts
+++ packages/commands/src/solana/index.ts
@@ -3,9 +3,8 @@ import { Command } from "commander";
 import { depositCommand } from "./deposit";
 import { encodeCommand } from "./encode";
 
-export const solanaCommand = new Command("solana").description(
-  "Solana commands"
-);
-
-solanaCommand.addCommand(depositCommand);
-solanaCommand.addCommand(encodeCommand).helpCommand(false);
+export const solanaCommand = new Command("solana")
+  .description("Solana commands")
+  .addCommand(depositCommand)
+  .addCommand(encodeCommand)
+  .helpCommand(false);
