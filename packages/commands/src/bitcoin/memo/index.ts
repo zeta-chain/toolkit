8f56e297d3d59ca2697702ee9bdb6ba0b27731d1
Denis Fadeev <denis@fadeev.org>
2025-05-23 20:20:18 +0300

    remove memo encode command

--- packages/commands/src/bitcoin/memo/index.ts
+++ packages/commands/src/bitcoin/memo/index.ts
@@ -3,7 +3,6 @@ import { Command } from "commander";
 import { callCommand } from "./call";
 import { depositCommand } from "./deposit";
 import { depositAndCallCommand } from "./depositAndCall";
-import { encodeCommand } from "./encode";
 
 export const memoCommand = new Command("memo")
   .description("Make a transaction using a memo (OP_RETURN)")
@@ -11,5 +10,4 @@ export const memoCommand = new Command("memo")
   .addCommand(callCommand)
   .addCommand(depositAndCallCommand)
   .addCommand(depositCommand)
-  .addCommand(encodeCommand)
   .helpCommand(false);
