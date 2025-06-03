c9cee541af0720d0b53336cb0d46dc770e3f5d93
Denis Fadeev <denis@fadeev.org>
2025-06-03 16:56:25 +0300

    lint

--- types/ton.types.ts
+++ types/ton.types.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 export const depositOptionsSchema = z.object({
   amount: z.string(),
   apiKey: z.string().optional(),
-  rpc: z.string(),
   gateway: z.string(),
   mnemonic: z.string(),
   receiver: z
@@ -12,13 +11,13 @@ export const depositOptionsSchema = z.object({
       /^0x[0-9a-fA-F]{40}$/,
       "EVM address must be 0x-prefixed 20-byte hex"
     ),
+  rpc: z.string(),
 });
 
 export const depositAndCallOptionsSchema = z.object({
   amount: z.string(),
   apiKey: z.string().optional(),
   data: z.string().optional(),
-  rpc: z.string(),
   gateway: z.string(),
   mnemonic: z.string(),
   receiver: z
@@ -27,6 +26,7 @@ export const depositAndCallOptionsSchema = z.object({
       /^0x[0-9a-fA-F]{40}$/,
       "EVM address must be 0x-prefixed 20-byte hex"
     ),
+  rpc: z.string(),
   types: z.array(z.string()).optional(),
   values: z.array(z.string()).optional(),
 });
