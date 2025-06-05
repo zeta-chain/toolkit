b35a0d5cda7204ab58d1a9458ae729c26f2c0809
Hernan Clich <hernan.g.clich@gmail.com>
2025-06-05 11:05:55 -0300

    Remove redundant refine rule

--- utils/solana.commands.helpers.ts
+++ utils/solana.commands.helpers.ts
@@ -10,20 +10,16 @@ import {
   SOLANA_TOKEN_PROGRAM,
 } from "../types/shared.constants";
 
-export const solanaDepositOptionsSchema = z
-  .object({
-    amount: z.string(),
-    mint: z.string().optional(),
-    mnemonic: z.string().optional(),
-    name: z.string().optional(),
-    network: z.string(),
-    privateKey: z.string().optional(),
-    recipient: z.string(),
-    tokenProgram: z.string().default(SOLANA_TOKEN_PROGRAM),
-  })
-  .refine((data) => !(data.mnemonic && data.privateKey), {
-    message: "Only one of mnemonic or privateKey can be provided, not both",
-  });
+export const solanaDepositOptionsSchema = z.object({
+  amount: z.string(),
+  mint: z.string().optional(),
+  mnemonic: z.string().optional(),
+  name: z.string().optional(),
+  network: z.string(),
+  privateKey: z.string().optional(),
+  recipient: z.string(),
+  tokenProgram: z.string().default(SOLANA_TOKEN_PROGRAM),
+});
 
 export const keypairFromMnemonic = async (
   mnemonic: string
