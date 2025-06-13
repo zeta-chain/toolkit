a45c740f5644d923dce7144944b6880aad0571fc
Denis Fadeev <denis@fadeev.org>
2025-06-13 18:28:16 +0300

    typesAndValuesLengthRefineRule

--- types/ton.types.ts
+++ types/ton.types.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 import { DEFAULT_ACCOUNT_NAME } from "./shared.constants";
-import { evmAddressSchema, validAmountSchema } from "./shared.schema";
+import {
+  evmAddressSchema,
+  typesAndValuesLengthRefineRule,
+  validAmountSchema,
+} from "./shared.schema";
 
 export const baseTonOptionsSchema = z.object({
   apiKey: z.string().optional(),
@@ -16,9 +20,14 @@ export const depositOptionsSchema = baseTonOptionsSchema.extend({
   amount: validAmountSchema,
 });
 
-export const depositAndCallOptionsSchema = baseTonOptionsSchema.extend({
-  amount: validAmountSchema,
-  data: z.string().optional(),
-  types: z.array(z.string()).optional(),
-  values: z.array(z.string()).optional(),
-});
+export const depositAndCallOptionsSchema = baseTonOptionsSchema
+  .extend({
+    amount: validAmountSchema,
+    data: z.string().optional(),
+    types: z.array(z.string()).optional(),
+    values: z.array(z.string()).optional(),
+  })
+  .refine(typesAndValuesLengthRefineRule.rule, {
+    message: typesAndValuesLengthRefineRule.message,
+    path: typesAndValuesLengthRefineRule.path,
+  });
