7c9c9576c0cc11228c045ae5d60d377729df43a4
Denis Fadeev <denis@fadeev.org>
2025-06-30 20:17:27 +0300

    feat: fees command

--- packages/commands/src/query/index.ts
+++ packages/commands/src/query/index.ts
@@ -2,9 +2,12 @@ import { Command } from "commander";
 
 import { balancesCommand } from "./balances";
 import { cctxCommand } from "./cctx";
+import { feesCommand } from "./fees";
 
 export const queryCommand = new Command("query")
+  .alias("q")
   .description("Query commands")
   .addCommand(balancesCommand)
   .addCommand(cctxCommand)
+  .addCommand(feesCommand)
   .helpCommand(false);
