f0061bb859e1c86e13ae360a3b86698c2bcd9f9e
Denis Fadeev <denis@fadeev.org>
2025-09-09 15:28:49 +0300

    refactor: get EVM RPC from Viem instead of @zetachain/networks

--- utils/chains.ts
+++ utils/chains.ts
@@ -1,53 +1,15 @@
-import { networks } from "@zetachain/networks";
-import { type NetworksSchema } from "@zetachain/networks/dist/src/types";
-
-export const getChainName = (chainId: number): string => {
-  const typedNetworks = networks as NetworksSchema;
-  const network = Object.values(typedNetworks).find(
-    (n) => n.chain_id === chainId
-  );
-
-  if (!network) {
-    throw new Error(`Network with chain ID ${chainId} not found`);
-  }
-
-  return network.chain_name;
-};
+import * as viemChains from "viem/chains";
 
 export const getRpcUrl = (chainId: number): string => {
-  const typedNetworks = networks as NetworksSchema;
-  const network = Object.values(typedNetworks).find(
-    (n) => n.chain_id === chainId
-  );
+  const chain = Object.values(viemChains).find((c) => c.id === chainId);
 
-  if (!network) {
-    throw new Error(`Network with chain ID ${chainId} not found`);
+  if (!chain) {
+    throw new Error(`Chain with ID ${chainId} not found in viem/chains`);
   }
 
-  if (!network.api) {
-    throw new Error(`Network with chain ID ${chainId} has no API endpoints`);
+  const url = chain.rpcUrls?.default?.http?.[0];
+  if (!url) {
+    throw new Error(`No RPC HTTP URL found for chain ID ${chainId}`);
   }
-
-  const evmRpc = network.api.find((api) => api.type === "evm");
-  if (!evmRpc) {
-    throw new Error(`Network with chain ID ${chainId} has no EVM RPC endpoint`);
-  }
-
-  return evmRpc.url;
-};
-
-export const getNetworkTypeByChainId = (
-  chainId: number
-): "mainnet" | "testnet" => {
-  const typedNetworks = networks as NetworksSchema;
-  const network = Object.values(typedNetworks).find(
-    (n) => n.chain_id === chainId
-  );
-
-  if (!network?.type) {
-    throw new Error(`Network with chain ID ${chainId} not found`);
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return network.type;
+  return url;
 };
