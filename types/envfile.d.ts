796b94eb276e6b645cf7b2dee6661aab9aeed077
Hernan Clich <hernan.g.clich@gmail.com>
2025-09-22 12:23:47 -0300

    Remove envfile d ts file

--- types/envfile.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'envfile' {
-  export function parseFileSync(path: string): Record<string, string>;
-  export function stringifySync(obj: Record<string, string>): string;
-  export function parse(content: string): Record<string, string>;
-  export function stringify(obj: Record<string, string>): string;
-}
