a10b4351ead53c1441f6f742f4c6bd37d36945c0
Denis Fadeev <denis@fadeev.org>
2025-09-30 20:22:58 +0300

    undelegate

--- packages/commands/src/staking/index.ts
+++ packages/commands/src/staking/index.ts
@@ -3,10 +3,12 @@ import { Command } from "commander";
 import { delegateCommand } from "./delegate";
 import { delegationsCommand } from "./delegations";
 import { validatorsCommand } from "./validators";
+import { undelegateCommand } from "./undelegate";
 
 export const stakingCommand = new Command("staking")
   .summary("Staking commands")
   .addCommand(validatorsCommand)
   .addCommand(delegateCommand)
+  .addCommand(undelegateCommand)
   .addCommand(delegationsCommand)
   .helpCommand(false);
