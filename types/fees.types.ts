88f4bece1523f20b7b83f196a0cecb961c281eae
Denis Fadeev <denis@fadeev.org>
2025-10-03 16:58:08 +0300

    zod

---
+++ types/fees.types.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+import { evmAddressSchema } from "./shared.schema";
+
+const chainMetaSchema = z.object({
+  id: z.string().optional(),
+  name: z.string().optional(),
+});
+
+const gasSchema = z.object({
+  chain: chainMetaSchema,
+  decimals: z.number(),
+  fee: z.string(),
+  symbol: z.string(),
+  zrc20: evmAddressSchema,
+});
+
+const sourceSchema = z.object({
+  amount: z.string(),
+  chain: chainMetaSchema,
+  decimals: z.number(),
+  equalsGas: z.boolean().optional(),
+  symbol: z.string(),
+  zrc20: evmAddressSchema,
+});
+
+export const showFeesDataSchema = z.object({
+  gas: gasSchema,
+  source: sourceSchema.optional(),
+});
